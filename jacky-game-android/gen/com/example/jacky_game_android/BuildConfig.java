ee55e57f5c8ecad503c21cb971b67935b6a7272f
jacky14 <277082264@qq.com>
2016-06-23 17:18:16 +0800

    android game

---
+++ jacky-game-android/gen/com/example/jacky_game_android/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.jacky_game_android;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ jacky-game-android/src/com/example/jacky_game_android/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.jacky_game_android;
+
+import android.os.Bundle;
+import android.view.KeyEvent;
+
+import com.jacky.android.JackyActivity;
+
+public class MainActivity extends JackyActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onResume (){
+        super.onResume();
+       
+    }
+     
+    @Override
+    protected void onPause (){
+        super.onPause();
+		
+    }
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		
+	}
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event){
+		return super.onKeyDown(keyCode, event);
+	}
+   
+    
+}
