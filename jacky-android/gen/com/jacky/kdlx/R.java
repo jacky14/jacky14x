8fce08c670591c200baa6c0c9cb34d6dd220de7d
jacky14 <277082264@qq.com>
2016-06-28 11:02:31 +0800

    删除安卓部分无用资源

--- jacky-android/gen/com/jacky/kdlx/R.java
+++ jacky-android/gen/com/jacky/kdlx/R.java
@@ -10,10 +10,7 @@ package com.jacky.kdlx;
 public final class R {
     public static final class attr {
     }
-    public static final class drawable {
-        public static int icon=0x7f020000;
-    }
     public static final class string {
-        public static int app_name=0x7f030000;
+        public static int app_name=0x7f020000;
     }
 }
