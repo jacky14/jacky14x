be1b887574a9a501f73e981b0d1e78545efd13a5
jacky14 <277082264@qq.com>
2016-06-28 11:03:41 +0800

    删除安卓部分支援

--- jacky-game-android/gen/com/jacky/kdlx/R.java
+++ jacky-game-android/gen/com/jacky/kdlx/R.java
@@ -7,9 +7,6 @@
 package com.jacky.kdlx;
 
 public final class R {
-	public static final class drawable {
-		public static final int icon = 0x7f020001;
-	}
 	public static final class string {
 		public static final int app_name = 0x7f030000;
 	}
