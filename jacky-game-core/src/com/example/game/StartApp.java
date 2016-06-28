753f836bae4270ccda84d2c2b9898b0cbe242c76
jacky14 <277082264@qq.com>
2016-06-28 09:24:00 +0800

    游戏模块中修改一些参数

--- jacky-game-core/src/com/example/game/StartApp.java
+++ jacky-game-core/src/com/example/game/StartApp.java
@@ -6,5 +6,7 @@ import com.testgame.GameScene;
 public class StartApp {
 	public static void setFirstScene(){
 		AppDelegate.share().first = new GameScene();
+		AppDelegate.design_high = 1136;
+		AppDelegate.design_width = 640;
 	}
 }
--- jacky-game-desktop/src/com/exmp/desk/AppStart.java
+++ jacky-game-desktop/src/com/exmp/desk/AppStart.java
@@ -5,9 +5,12 @@ import com.jacky.GameStart;
 
 public class AppStart {
 	public static void main(String[] args) {
-		
-		
 		GameStart gameStart = new GameStart();
+		gameStart.width = 850;
+		gameStart.high = 480;
+		gameStart.x = 100;
+		gameStart.y = 100;
+		
 		StartApp.setFirstScene();
 		gameStart.start();
 	}
